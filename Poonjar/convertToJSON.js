b6f49c9c5f4fbb678d0f67f0cc4050475dcc6c29
Cource <jeffjacobjoy@gmail.com>
2020-07-31 18:38:59 +0530

    entho sadanam

--- Poonjar/convertToJSON.js
+++ Poonjar/convertToJSON.js
@@ -11,28 +11,27 @@ const fs = require('fs')
 
 function csvJSON(csv){
 
-  let lines =csv.split("\n");
+    let lines =csv.split("\n");
 
-  let result = [];
-  let headers=['Name', "guardian", 'HouseNo', 'HouseName', 'Gender_Age', 'IDCardNo'];
+    let result = [];
+    let headers=['name', "guardian", 'houseNumber', 'houseName', 'gender', 'voterId'];
 
-  for(let i in lines){
-      if ()
-      let obj = {};
-      let currentline=lines[i].split(",");
+    for(let i in lines){
+        if (lines[i] !== ''){
+            let currentline=lines[i].split(",");
+            let obj = {};
 
-      for(let j in headers){
-          obj[headers[j]] = currentline[j];
-      }
+            for(let j in headers){
+                obj[headers[j]] = currentline[j];
+            }
 
-      result.push(obj);
-
+            result.push(obj);
+    }
   }
 
   return JSON.stringify(result);
 }
-fs.readFile('Poonjar/G05036-Poonjar001-PERUNNILAM001-PERUNNILAM ANGANVADI  GROUND FLOOR.csv', (err, res)=>
+fs.readFile('Poonjar/G05036-Poonjar002-MATTAYKKADU001-POONJAR GOVT. LP SCHOOL, 2ND BLOCK  SOUTH PART.csv', (err, res)=>
 {
-    console.log(res)
     console.log(csvJSON(res.toString()))
 })

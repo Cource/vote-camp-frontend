1e22ad65c6c93f0733510a59e2fe0f1bbb7566d2
Cource <jeffjacobjoy@gmail.com>
2020-11-02 14:11:24 +0530

    Design changes

--- components/optionPicker.tsx
+++ components/optionPicker.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default (props: any) => {
     return (
-        <View style={props.style} >
+        <View style={[props.style, styles.option]} >
             <Text style={[styles.optionTitle, props.titleStyle]}>{props.title}</Text>
             <View style={{ borderRadius: 7, borderColor: "#555", borderStyle: "solid", borderWidth: 2 }}>
                 <Picker
@@ -31,8 +31,17 @@ export default (props: any) => {
 }
 
 const styles = StyleSheet.create({
+    option: {
+        marginVertical: 5
+    },
     optionTitle: {
-        fontSize: 20,
         fontWeight: "bold",
+        zIndex: 10,
+        alignSelf: "flex-start",
+        fontSize: 15,
+        marginLeft: 5,
+        marginBottom: -10,
+        backgroundColor: '#f0f0f0',
+        paddingHorizontal: 5
     },
 })
